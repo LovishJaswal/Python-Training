dc504717b7b3a2fc14a64b6ae951a64f33208126
Lovish <lovishjaswal01@gmail.com>
2026-06-07 06:42:02 +0530

    Add attempt counter to Number Guessing Game

--- Mini Projects/Number-Guessing-Game.py
+++ Mini Projects/Number-Guessing-Game.py
@@ -3,9 +3,11 @@ print("\nWelcome to the number guessing game\n")
 num = random.randrange(1, 101)
 print("Computer has generated a random number(Between 1-100)")
 guess = None
+cnt = 0
 while guess != num:
     guess = int(input("Try guessing it here: "))
     diff = abs(guess - num)
+    cnt+=1
     if diff < 10 and guess < num:
         print("You're almost there! Go a bit higher")
     elif diff < 10 and guess > num:
@@ -21,3 +23,4 @@ while guess != num:
     else:
         print("\nCongratulations!, you're spot on")
         print("You have guessed the number correctly")
+        print(f"Number of tries: {cnt}")
